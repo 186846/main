d4d7b260cad595694e6aba34d443059ddb8073b1
186846 <93398636+186846@users.noreply.github.com>
2021-10-30 01:08:04 +0800

    Add files via upload

---
+++ 干行表现.c
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main()
+{
+	int i;
+    int b[]={32,32,206,210,207,178,187,182,196,227,33,32,32};
+    for(i=0;i<13;i++)
+    {
+        printf("%c",b[i]);
+    }
+    printf("\n");
+    return 0;
+}
